fbec9730a0b10a10fde973a9cd33a5b17825ba2a
Nasif Fuad <nasifuad007@gmail.com>
2024-08-03 21:23:25 +0600

    1460 1460. Make Two Arrays Equal by Reversing Subarrays

---
+++ 1460.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} target
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+
+var canBeEqual = function (target, arr) {
+    target.sort()
+    arr.sort()
+
+    for (let i = 0; i <= arr.length - 1; i++) {
+        if (arr[i] != target[i]) {
+            return false
+        }
+        return true
+    }
+
+
+};
